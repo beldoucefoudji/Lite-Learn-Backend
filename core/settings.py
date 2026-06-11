345cd03886ecdf7c17ca0dd1f5b1c58b3c8e770a
beldoucefoudji <beldoucefoudji@gmail.com>
2026-06-11 11:16:46 +0200

    fix: set whitenoise manifest strict to false to ignore missing theme source

--- core/settings.py
+++ core/settings.py
@@ -157,6 +157,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
