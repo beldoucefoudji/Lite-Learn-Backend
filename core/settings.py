1519b43cde6ef4755eea523a680bd61e58d4bfdf
beldoucefoudji <beldoucefoudji@gmail.com>
2026-06-11 12:23:18 +0200

    fix: swap to standard compressed static files storage for whitenoise

--- core/settings.py
+++ core/settings.py
@@ -154,14 +154,14 @@ USE_TZ = True
 
 
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_MANIFEST_STRICT = False  
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if not DEBUG:
