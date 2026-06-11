6f1fb18a8fca17936c654e9ba2b69c0740a7a29f
beldoucefoudji <beldoucefoudji@gmail.com>
2026-06-11 10:53:33 +0200

    chore: add whitenoise static files storage setting

--- core/settings.py
+++ core/settings.py
@@ -156,6 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
